5a5a252749535e537c943a8c84e17428c925e144
David Abreu <david.abreu@outlook.com>
2021-12-06 21:50:43 -0300

    imghelper_main.py: ready imghelper_lib.py:   - ready: parse_path(), rotate_img(), resize_img()   - TODO: change_format()

--- imghelper_main.py
+++ imghelper_main.py
@@ -1,5 +1,6 @@
 #!/home/dvlinux/PycharmProjects/img_helper/venv/bin/python3.10
 from lib.imghelper_lib import *
+from PIL import Image
 
 
 def parse_arguments():
@@ -8,6 +9,7 @@ def parse_arguments():
     add_argument = parser.add_argument
     add_argument('-r',
                  dest="rotate",
+                 choices=["90", "180", "270"],
                  action='store',
                  nargs=1,
                  )
@@ -17,6 +19,7 @@ def parse_arguments():
                  nargs=2,
                  )
     add_argument('--path',
+                 dest="path",
                  action='store',
                  nargs=1,
                  default="images/"
@@ -36,15 +39,26 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
+    path = parse_path(args.path[0])
+    image_obj_list = [(Image.open(file), path) for file in path]
+    img_path = os.path.join(os.path.dirname(path[0]), "imghelper")
 
     if args.rotate:
-        rotate_img(args.rotate)
-    elif args.size:
-        resize_img(args.size)
-    elif args.save:
-        change_format(args.save)
-    else:
-        print("Opção inválida")
+        image_obj_list = rotate_img(args.rotate, image_obj_list)
+    if args.size:
+        image_obj_list = resize_img(args.size, image_obj_list)
+    if args.save:
+        image_obj_list = change_format(args.save, image_obj_list)
+
+    if not os.path.exists(img_path):
+        os.mkdir(img_path)
+
+    for index, image in enumerate(image_obj_list):
+        image_obj = image[0]
+        image_path = image[1]
+        print(image_path)
+        image_obj.save(os.path.join(img_path, os.path.basename(image_path[index])))
+        image_obj.close()
 
 
 if __name__ == "__main__":
--- lib/imghelper_lib.py
+++ lib/imghelper_lib.py
@@ -1,14 +1,41 @@
-from PIL import Image
-import sys, os, argparse
+import os
+import sys
 
 
-def rotate_img(args):
-    print(args)
+def parse_path(path):
+    if os.path.isabs(path):
+        if os.path.exists(path):
+            if os.path.isfile(path):
+                return [path]
+            if os.path.isdir(path):
+                return [os.path.abspath(file) for file in os.listdir(path) if os.path.isfile(file)]
+        else:
+            print("file not found")
+            sys.exit(1)
+    else:
+        abs_path = os.path.join(os.getcwd(), path)
+        if os.path.exists(abs_path):
+            if os.path.isfile(abs_path):
+                return [abs_path]
+            if os.path.isdir(abs_path):
+                return [os.path.abspath(file) for file in os.listdir(path) if os.path.isfile(file)]
+        else:
+            print("file not found")
+            sys.exit(1)
 
 
-def resize_img(args):
-    print(args)
+def rotate_img(angle_arg, img_obj_list):
+    angle = int(angle_arg[0])
+    img_obj_list = [(img_obj.rotate(angle, expand=True), path) for img_obj, path in img_obj_list]
+    return img_obj_list
 
 
-def change_format(args):
-    print(args)
+def resize_img(sizes, img_obj_list):
+    height, width = int(sizes[0]), int(sizes[1])
+    new_size = (height, width)
+    img_obj_list = [(img_obj.resize(new_size), path) for img_obj, path in img_obj_list]
+    return img_obj_list
+
+
+def change_format(new_format, img_obj_list):
+    print(new_format)
