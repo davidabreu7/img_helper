67d79904c809efa4849f0c09a092f96e6d28e76f
David Abreu <david.abreu@outlook.com>
2021-12-09 20:07:19 -0300

    lots of bugfixes for parse_path(), and saving files

--- imghelper_main.py
+++ imghelper_main.py
@@ -21,44 +21,61 @@ def parse_arguments():
     add_argument('--path',
                  dest="path",
                  action='store',
-                 nargs=1,
-                 default="images/"
+                 nargs="+",
+                 required=True,
                  )
-    add_argument('-e',
-                 dest="save",
-                 action='store',
-                 nargs=1,
+    add_argument('--jpg',
+                 dest="jpg",
+                 action='store_true',
                  )
-    add_argument('-n',
+    add_argument('--prefix ',
                  dest='name',
                  action="store",
-                 nargs=1,
+                 nargs="?",
+                 )
+    add_argument('--suffix ',
+                 dest='name',
+                 action="store",
+                 nargs="?",
                  )
     return parser.parse_args()
 
 
+def save_images(paths, image_obj_list):
+
+    try:
+        for index, file in enumerate(image_obj_list):
+                file.save(paths.save[index])
+                file.close()
+        return True
+    except OSError:
+        print(f"Could not save file{OSError}")
+        sys.exit(1)
+    except ValueError:
+        print(f"Could not save file: {ValueError}")
+        sys.exit(1)
+
+
 def main():
     args = parse_arguments()
-    path = parse_path(args.path[0])
-    image_obj_list = [(Image.open(file), path) for file in path]
-    img_path = os.path.join(os.path.dirname(path[0]), "imghelper")
+    path = parse_path(args.path)
+
+    try:
+        image_obj_list = [Image.open(file) for file in path.read]
+    except OSError:
+        print(f"Could not read files in dir {os.path.dirname(path.read[0])}")
+        sys.exit(1)
 
     if args.rotate:
         image_obj_list = rotate_img(args.rotate, image_obj_list)
     if args.size:
         image_obj_list = resize_img(args.size, image_obj_list)
-    if args.save:
-        image_obj_list = change_format(args.save, image_obj_list)
+    if args.jpg:
+        image_obj_list = change_format(args.jpg, image_obj_list)
 
-    if not os.path.exists(img_path):
-        os.mkdir(img_path)
-
-    for index, image in enumerate(image_obj_list):
-        image_obj = image[0]
-        image_path = image[1]
-        print(image_path)
-        image_obj.save(os.path.join(img_path, os.path.basename(image_path[index])))
-        image_obj.close()
+    if save_images(path, image_obj_list):
+        print(f"Images saved in dir {os.path.dirname(path.save[0])}")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
--- lib/imghelper_lib.py
+++ lib/imghelper_lib.py
@@ -1,41 +1,72 @@
 import os
 import sys
+from collections import namedtuple
 
 
-def parse_path(path):
-    if os.path.isabs(path):
-        if os.path.exists(path):
-            if os.path.isfile(path):
-                return [path]
-            if os.path.isdir(path):
-                return [os.path.abspath(file) for file in os.listdir(path) if os.path.isfile(file)]
-        else:
-            print("file not found")
-            sys.exit(1)
+def parse_path(paths: list[str]) -> namedtuple:
+    # creating namedtuple with paths
+    ImagePath = namedtuple("ImagePath", "read save")
+
+    # parsing ImagePath.read
+    ImagePath.read = os.path.abspath(paths[0])
+
+    if os.path.exists(ImagePath.read):
+        if os.path.isfile(ImagePath.read):
+            ImagePath.read = [os.path.abspath(ImagePath.read)]
+        if os.path.isdir(ImagePath.read[0]):
+            dir_name = ImagePath.read
+            list_dir = os.listdir(ImagePath.read)
+            ImagePath.read = [os.path.join(dir_name, file) for file in list_dir if
+                              os.path.isfile(os.path.join(dir_name, file))]
     else:
-        abs_path = os.path.join(os.getcwd(), path)
-        if os.path.exists(abs_path):
-            if os.path.isfile(abs_path):
-                return [abs_path]
-            if os.path.isdir(abs_path):
-                return [os.path.abspath(file) for file in os.listdir(path) if os.path.isfile(file)]
+        print("file not found")
+        sys.exit(1)
+
+    # parsing ImagePath.save
+    if len(paths) < 2:
+        if os.path.isfile(paths[0]):
+            ImagePath.save = [os.path.join(os.path.abspath(os.path.dirname(ImagePath.read[0])), "imghelper/",
+                                           os.path.basename(paths[0]))]
         else:
-            print("file not found")
-            sys.exit(1)
+            ImagePath.save = [os.path.join(os.path.abspath(paths[0]), "imghelper/")]
+
+    else:
+        ImagePath.save = [paths[1]]
+        print(ImagePath.save)
+
+    if not os.path.exists(ImagePath.save[0]):
+        if os.path.sep == ImagePath.save[0][-1]:
+            file_save_name = [os.path.basename(file) for file in ImagePath.read]
+            dir_save_name = ImagePath.save[0]
+            os.mkdir(ImagePath.save[0])
+            ImagePath.save = [os.path.join(dir_save_name, file) for file in file_save_name]
+    else:
+        file_save_name = [os.path.basename(file) for file in ImagePath.read]
+        dir_save_name = ImagePath.save[0]
+        ImagePath.save = [os.path.join(dir_save_name, file) for file in file_save_name]
+    return ImagePath
 
 
 def rotate_img(angle_arg, img_obj_list):
     angle = int(angle_arg[0])
-    img_obj_list = [(img_obj.rotate(angle, expand=True), path) for img_obj, path in img_obj_list]
+    img_obj_list = [img_obj.rotate(angle, expand=True) for img_obj in img_obj_list]
     return img_obj_list
 
 
 def resize_img(sizes, img_obj_list):
     height, width = int(sizes[0]), int(sizes[1])
     new_size = (height, width)
-    img_obj_list = [(img_obj.resize(new_size), path) for img_obj, path in img_obj_list]
+    img_obj_list = [img_obj.resize(new_size) for img_obj in img_obj_list]
     return img_obj_list
 
 
 def change_format(new_format, img_obj_list):
-    print(new_format)
+    pass
+
+
+def add_prefix(prefix: list[str], img_ob_list):
+    pass
+
+
+def add_suffix(prefix: list[str], img_ob_list):
+    pass
